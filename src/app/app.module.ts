5fbf4c297a8c7f948f32376c30598e644e6b558d
enrfercas <114150760+enrfercas@users.noreply.github.com>
2024-03-27 12:38:14 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public form: FormGroup;
+  public isLoggedIn: boolean = false;
+
+  constructor(private fb: FormBuilder, private router: Router, /* private authService: AuthService */){
+    this.form = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]]
+    });
+  }
+
+  ngOnInit(): void {
+
+    /* this.authService.isLogged.subscribe((res) => {
+      this.isLoggedIn = res;
+    }) */
+
+  }
+
+  async onSubmit(formValue: FormGroup): Promise<void> {
+    /* if(this.form.valid) {
+      console.log(formValue);
+
+      const res = await this.authService.onLoggIn(formValue).then((data: any) =>{
+
+        console.log("Subscripción desde el componente",data);
+
+        if((this.isLoggedIn)){
+          if((this.authService.roles.includes("admin")) || (this.authService.roles.includes("moderator"))) {
+
+            this.router.navigate(['owners']);
+          } else {
+            this.router.navigate(['']);
+          }
+        }else {
+          alert("Wrong password or username");
+        }
+      });
+      console.log("El token desde el componente: ",this.authService.token);
+      console.log("IsLogged: ",this.authService.isLoggedIn);
+      console.log("El role desde el componente: ",this.authService.roles);
+
+    }*/
+  }
+}
